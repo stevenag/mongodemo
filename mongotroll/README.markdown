c3e59daeb2470a069a324ec0dd7c454468135727
stevenag <steven.al.gu@gmail.com>
2015-06-05 03:22:05 -0500

    asdf

---
+++ mongotroll/README.markdown
@@ -0,0 +1,5 @@
+# PHP MongoDB Admin
+
+A MongoDB Admin interface that runs in your web browser.
+
+Open mongodbadmin.php in your browser and have a look!
---
+++ prueba.php
@@ -0,0 +1,28 @@
+<?php
+
+// conectar
+$m = new MongoClient();
+
+// seleccionar una base de datos
+$bd = $m->miprimerabd;
+
+// seleccionar una colección (equivalente a una tabla en una base de datos relacional)
+$colección = $bd->noticias;
+
+// añadir un registro
+//$documento = array( "title" => "Calvin and Hobbes", "author" => "Bill Watterson" );
+//$colección->insert($documento);
+
+// añadir un nuevo registro, con un distinto "perfil"
+//$documento = array( "title" => "XKCD", "online" => true );
+//$colección->insert($documento);
+
+// encontrar todo lo que haya en la colección
+$cursor = $colección->find();
+
+// recorrer el resultado
+foreach ($cursor as $documento) {
+    echo $documento["titulo"] . "\n";
+}
+
+?>
